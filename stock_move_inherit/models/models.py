170f9c6cd828d6f20fea6269beaa19d3368f5dbb
“Sunhyeok <suju@odoo.com>
2022-06-22 09:33:38 -0400

    [EDIT] stock_move_inherit : Now the warning appears only in receipts

--- stock_move_inherit/models/models.py
+++ stock_move_inherit/models/models.py
@@ -10,7 +10,7 @@ class stock_move_inherit(models.Model):
     _inherit = 'stock.move'
     @api.onchange('quantity_done')
     def _doing_something(self):
-        if self.quantity_done > self.product_uom_qty:
-            raise ValidationError("You can't receive more than the ordered quantity. Please, enter another quantity.")   
-        
+        if (self.picking_type_id.code == "incoming"): # check if this is receipt 
+            if self.quantity_done > self.product_uom_qty:
+                raise ValidationError("You can't receive more than the ordered quantity. Please, enter another quantity.")   
         
